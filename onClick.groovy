7884db10821d8e3ea6abb7b852ed79ad02182380
smonterrosog2 <100809150+smonterrosog2@users.noreply.github.com>
2022-03-08 15:30:00 -0600

    Update onClick.groovy

--- onClick.groovy
+++ onClick.groovy
@@ -73,4 +73,78 @@ public class MainActivity extends AppCompatActivity  {
 }
 
 
-/*******Opcion 2: multiples objetos onClick usando condicional if********/
+/*******Opcion 2: multiples objetos onClick usando condicional switch********/
+
+//activity_main.xml
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:padding="16dp"
+    android:orientation="vertical"
+    tools:context=".MainActivity">
+
+    <Button
+        android:id="@+id/button1"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Button1"
+        android:layout_gravity="center"
+        android:onClick="onClick"/>
+
+    <Button
+        android:id="@+id/button2"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Button2"
+        android:layout_gravity="center"
+        android:onClick="onClick"/>
+
+    <Button
+        android:id="@+id/button3"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Button3"
+        android:layout_gravity="center"
+        android:onClick="onClick"/>
+
+</LinearLayout>
+
+//MainSctivity.java
+package com.example.seccion1app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    //Opcion 2: Multiples objetos onClick usando condicional switch
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1:
+                Toast.makeText(this, "Ingreso con el botón 1", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this, "Ingreso con el botón 2", Toast.LENGTH_LONG).show();
+            case R.id.button3:
+                Toast.makeText(this, "Ingreso con el botón 3", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+     }
+     
+}
+
+
